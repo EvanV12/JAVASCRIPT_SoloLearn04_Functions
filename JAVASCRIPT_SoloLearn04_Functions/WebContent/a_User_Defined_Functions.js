ea57f6c84d413d4e067b1d4f834505f0115e1a74
EvanV12 <evangv12@gmail.com>
2019-08-04 18:36:03 +0300

    Added "User-Defined Functions"

---
+++ JAVASCRIPT_SoloLearn04_Functions/WebContent/a_User_Defined_Functions.js
@@ -0,0 +1,26 @@
+/*
+function name() {
+	// code to be executed
+	}
+*/
+
+
+function myFunction() {
+	alert("Callin a Function!");
+}
+
+myFunction();
+// Alerts "Calling a Function!"
+
+
+function myFunction2() {
+	alert("Alert box!");
+}
+
+myFunction2();
+// "Alert box!"
+
+// some other code
+
+myFunction2();
+//"Alert box!"
